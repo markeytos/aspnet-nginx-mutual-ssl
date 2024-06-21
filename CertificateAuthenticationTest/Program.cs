a41a9417a17f9343d26ace5c7480ce4f1dcae127
Alan Chung Ma <alan@keytos.io>
2024-06-21 11:15:49 -0400

    Run csharpier

--- CertificateAuthenticationTest/Program.cs
+++ CertificateAuthenticationTest/Program.cs
@@ -10,7 +10,6 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
     options.ForwardedHeaders = ForwardedHeaders.XForwardedProto;
 });
 
-
 builder.Services.AddCertificateForwarding(options =>
 {
     options.CertificateHeader = "X-SSL-CERT";
@@ -21,8 +20,9 @@ builder.Services.AddCertificateForwarding(options =>
     };
 });
 
-builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate(
-    options =>
+builder
+    .Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
+    .AddCertificate(options =>
     {
         options.AllowedCertificateTypes = CertificateTypes.All;
         options.ChainTrustValidationMode = X509ChainTrustMode.CustomRootTrust;
@@ -31,7 +31,12 @@ builder.Services.AddAuthentication(CertificateAuthenticationDefaults.Authenticat
         // Disabled revocation checks since test certificates do not have CRL.
         options.RevocationMode = X509RevocationMode.NoCheck;
     });
-builder.Services.AddAuthorizationBuilder().AddFallbackPolicy("RequireAuthenticatedUser", authorizationPolicyBuilder => authorizationPolicyBuilder.RequireAuthenticatedUser());
+builder
+    .Services.AddAuthorizationBuilder()
+    .AddFallbackPolicy(
+        "RequireAuthenticatedUser",
+        authorizationPolicyBuilder => authorizationPolicyBuilder.RequireAuthenticatedUser()
+    );
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
